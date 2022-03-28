7aac94e96e5eb3ef0664d15510c9118b96cfd57f
Oluwatoyin Amushan <alexandradonalds@gmail.com>
2022-03-28 23:53:28 +0100

    Code cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -95,32 +95,6 @@ const Login = () => {
           id="password"
           value={passState.value}
           onChange={passwordChangeHandler}/>
-        {/* <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-          />
-        </div> */}
-        {/* <div
-          className={`${classes.control} ${
-            passState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passState.value}
-            onChange={passwordChangeHandler}
-          />
-        </div> */}
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
